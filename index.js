f7706dec54c13b027a48a824ebd607cf7bdd35a2
pirushanp <pirushanp@gmail.com>
2017-11-09 18:53:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
+<<<<<<< HEAD
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
+=======
+ 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+ 
+>>>>>>> 198a002e0feb2b7080f3791932e51a5b07ed6102
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
